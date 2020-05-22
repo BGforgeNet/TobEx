c203f205716b8d872ed5b9f0659c2043e7f2233f
snow <snowcanbe@gmail.com>
2020-05-22 17:46:46 +0700

    fix crash

--- Common/detoursext.h
+++ Common/detoursext.h
@@ -2,26 +2,31 @@
 #define DETOURSEXT_H
 
 #include "detours.h"
+
 template <typename RET_TYPE, typename CLASS, typename...ARGs>
-void* castToVoidPtr(RET_TYPE(CLASS::*&&pOriginalFunction)(ARGs...))
+void* castToVoidPtr(RET_TYPE (CLASS::*&&pOriginalFunction)(ARGs ...))
 {
-	auto voidPtr = reinterpret_cast<void*>(&pOriginalFunction);
-	static_assert(sizeof(&pOriginalFunction) == sizeof(voidPtr), "Cannot cast this member function pointer to a void*.  Not the same size.");
+	PVOID iter1 = reinterpret_cast<PVOID>(&pOriginalFunction);
+	PVOID voidPtr = reinterpret_cast<PVOID>(*(PVOID *)iter1);
+	static_assert(sizeof(&pOriginalFunction) == sizeof(voidPtr),
+		"Cannot cast this member function pointer to a void*.  Not the same size.");
 	return voidPtr;
 }
 
-// Cast a member function pointer to a void*&
-template <typename RET_TYPE, typename CLASS, typename...ARGs>
-void*& castToVoidPtr(RET_TYPE(CLASS::*&pOriginalFunction)(ARGs...))
-{
-	
-	auto voidPtr = reinterpret_cast<void*&>(&pOriginalFunction);
-	static_assert(sizeof(&pOriginalFunction) == sizeof(voidPtr), "Cannot cast this member function pointer to a void*.  Not the same size.");
-	return voidPtr;
-}
+//
+// // Cast a member function pointer to a void*&
+// template <typename RET_TYPE, typename CLASS, typename...ARGs>
+// void*& castToVoidPtr(RET_TYPE(CLASS::*&pOriginalFunction)(ARGs...))
+// {
+// 	
+// 	auto voidPtr = reinterpret_cast<void*&>(&pOriginalFunction);
+// 	static_assert(sizeof(&pOriginalFunction) == sizeof(voidPtr), "Cannot cast this member function pointer to a void*.  Not the same size.");
+// 	return voidPtr;
+// }
 
+//#define DetourMemberFunction(target, detour) DetourAttach(&(PVOID&)target, (PVOID)(&(PVOID&)detour));
 #define DetourFunction(target, detour) DetourAttach(&(PVOID&)target, (PVOID)detour);
-#define DetourMemberFunction(target, detour) DetourAttach(&(PVOID&)target, (PVOID)castToVoidPtr(&detour));
+#define DetourMemberFunction(target, detour) DetourAttach(&(PVOID&)target, castToVoidPtr(&detour));
 
 #define DetourMemberStaticFunction(target, detour) DetourAttach(&(PVOID&)target, &(PVOID&)detour);
 #endif //DETOURSEXT_H
